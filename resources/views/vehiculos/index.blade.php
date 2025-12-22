40945b07e4e385d96784edd145074cfc6b73f5e0
Alexis Bareiro <alexisblugo@gmail.com>
2025-12-22 17:57:25 -0300

    style: Add cursor-pointer class to the edit vehicle button.

--- resources/views/vehiculos/index.blade.php
+++ resources/views/vehiculos/index.blade.php
@@ -114,7 +114,8 @@
                                             </svg>
                                         </a>
                                         <button onclick="abrirModalEditarVehiculo({{ json_encode($vehiculo) }})"
-                                            class="text-gray-500 hover:text-blue-600 transition" title="Editar Vehículo">
+                                            class="cursor-pointer text-gray-500 hover:text-blue-600 transition"
+                                            title="Editar Vehículo">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
                                                 viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
