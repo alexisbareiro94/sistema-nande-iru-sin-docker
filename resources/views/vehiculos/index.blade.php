bd4b6c42899c0930181d2af4bf7690ba8b4eeca3
Alexis Bareiro <alexisblugo@gmail.com>
2026-01-13 09:06:13 -0300

    refactor: Remove max-width constraint from vehicle index container for improved responsiveness

--- resources/views/vehiculos/index.blade.php
+++ resources/views/vehiculos/index.blade.php
@@ -5,7 +5,7 @@
 @section('ruta-actual', 'Vehículos')
 
 @section('contenido')
-    <div class="max-w-7xl mx-auto">
+    <div class="">
         <!-- Header -->
         <div class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 gap-4">
             <div>
